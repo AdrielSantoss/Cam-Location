becf6b9d3ee7f12afacf62abed7d79019374ce8f
Asantoss <adrieldossantos7@gmail.com>
2022-01-16 17:41:42 -0300

    Adiciona widgets em main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,51 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:procjet/questao.dart';
+import 'package:procjet/resposta.dart';
 
 main() {
   runApp(const PerguntaApp()); 
 }
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp>  {
+  var perguntaSelecionada = 0; 
+  final perguntas = [
+    'Qual sua cor favorita?',
+    'Qual é o seu animal favorito?'
+  ];
+
+  void responder() {
+    setState(() => perguntaSelecionada++);
+  }
+
+    @override
+    Widget build(BuildContext context) { 
+      return MaterialApp(
+        home:  Scaffold(
+          appBar: AppBar(
+            title: const Text('Pergunta'),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: [
+                Questao(texto: perguntas[perguntaSelecionada]),
+                 Reposta(texto: 'Resposta 1', responder:responder),
+                 Reposta(texto: 'Resposta 2', responder:responder),
+                 Reposta(texto: 'Resposta 3', responder:responder),
+            ],
+          )
+        )
+      ); 
+    }
+}
+
+class PerguntaApp extends StatefulWidget {
   const PerguntaApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home:  Scaffold(
-        appBar: AppBar(
-          title: const Text('Pergunta'),
-          centerTitle: true,
-        ),
-        body: const Text('Olá Flutter'),
-      )
-    );
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 } 
