bb6d3681f6911f0a06d496498407fdd23b9e7a62
Asantoss <adrieldossantos7@gmail.com>
2022-01-19 21:40:58 -0300

    Adiciona iteração declarativa com Map() e imperativa com FOR().

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,18 @@ main() {
 class PerguntaAppState extends State<PerguntaApp>  {
   var perguntaSelecionada = 0; 
   final perguntas = [
-    'Qual sua cor favorita?',
-    'Qual é o seu animal favorito?'
+    {
+      'texto':'Qual sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco',]
+    },
+    {
+      'texto':'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
+    },
+    {
+      'texto': 'Qual sua linguagem favorita?',
+      'respostas': ['PHP', 'Javascript', 'C#', 'Java']
+    },
   ];
 
   void responder() {
@@ -22,6 +32,13 @@ class PerguntaAppState extends State<PerguntaApp>  {
 
     @override
     Widget build(BuildContext context) { 
+      List<String> respostas = temPerguntaSelecionada ? perguntas[perguntaSelecionada].cast()['respostas'] : [];
+  
+      //List<Widget> respostas = [];
+      // for (String textoResp in perguntas[perguntaSelecionada].cast()['respostas']) { // ITERAÇÃO IMPERATIVA
+      //   respostas.add(Reposta(texto:textoResp, responder:responder));
+      // }
+
       return MaterialApp(
         home:  Scaffold(
           appBar: AppBar(
@@ -30,10 +47,8 @@ class PerguntaAppState extends State<PerguntaApp>  {
           ),
           body: Column(
             children: [
-                Questao(texto: perguntas[perguntaSelecionada]),
-                 Reposta(texto: 'Resposta 1', responder:responder),
-                 Reposta(texto: 'Resposta 2', responder:responder),
-                 Reposta(texto: 'Resposta 3', responder:responder),
+                 Questao(texto: perguntas[perguntaSelecionada]['texto'].toString()),
+                 ...respostas.map((t) => Reposta(texto:t, responder:responder)).toList() // spread: todos elementos da lista vao ser adicionados aqui. -> map comunzao: ITERAÇÃO DELCARATIVA
             ],
           )
         )
