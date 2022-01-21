46da681f9be20a9aa894e4dd238d336c3b576555
Asantoss <adrieldossantos7@gmail.com>
2022-01-20 21:20:11 -0300

    Adiciona notas e novos componentes/Widgets em main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,72 @@
 // ignore_for_file: avoid_print
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:procjet/questao.dart';
-import 'package:procjet/resposta.dart';
+import 'package:procjet/questionario.dart';
+import 'package:procjet/resultado.dart';
 
 main() {
   runApp(const PerguntaApp()); 
 }
 
 class PerguntaAppState extends State<PerguntaApp>  {
-  var perguntaSelecionada = 0; 
-  final perguntas = [
-    {
-      'texto':'Qual sua cor favorita?',
-      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco',]
-    },
-    {
-      'texto':'Qual é o seu animal favorito?',
-      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
-    },
-    {
-      'texto': 'Qual sua linguagem favorita?',
-      'respostas': ['PHP', 'Javascript', 'C#', 'Java']
-    },
-  ];
+    var perguntaSelecionada = 0; 
+    var pontuacaoTotal = 0;
+    final perguntas = [
+      {
+        'texto':'Qual sua cor favorita?',
+        'respostas': [
+            {'texto':'Preto', 'nota':10}, 
+            {'texto':'Vermelho', 'nota':5}, 
+            {'texto':'Verde', 'nota':3}, 
+            {'texto':'Branco', 'nota':1}
+          ]
+      },
+      {
+        'texto':'Qual é o seu animal favorito?',
+        'respostas': [
+            {'texto':'Coelho', 'nota':2},
+            {'texto':'Cobra', 'nota':10},
+            {'texto':'Elefante', 'nota':8},
+            {'texto':'Leão', 'nota':0}
+          ]
+      },
+      {
+        'texto': 'Qual sua linguagem favorita?',
+        'respostas': [
+            {'texto':'PHP', 'nota':9}, 
+            {'texto':'Javascript', 'nota':8},
+            {'texto':'C#', 'nota':2},
+            {'texto': 'Java', 'nota':10}
+         ]
+      },
+    ];
 
-  void responder() {
-    if(temPerguntaSelecionada) {
-      setState(() => (perguntaSelecionada++));
+    void responder(int pontuacao) {
+      if(temPerguntaSelecionada) {
+        setState(() {
+              perguntaSelecionada++;
+              pontuacaoTotal += pontuacao;
+        });
+      }
+      print(pontuacaoTotal);
     }
-  }
 
-    bool get temPerguntaSelecionada { // getter
+    bool get temPerguntaSelecionada {
       return perguntaSelecionada < perguntas.length;
     }
 
     @override
-    Widget build(BuildContext context) {  
-
-      List<String> respostas = temPerguntaSelecionada ? perguntas[perguntaSelecionada].cast()['respostas'] : [];
-  
-      //List<Widget> respostas = [];
-      // for (String textoResp in perguntas[perguntaSelecionada].cast()['respostas']) { // ITERAÇÃO IMPERATIVA
-      //   respostas.add(Reposta(texto:textoResp, responder:responder));
-      // }
-
+    Widget build(BuildContext context) {    
       return MaterialApp(
         home:  Scaffold(
           appBar: AppBar(
             title: const Text('Pergunta'),
             centerTitle: true,
           ),
-          body: temPerguntaSelecionada ? Column( // condição para exibir um componente
-            children: [
-                 Questao(texto: perguntas[perguntaSelecionada]['texto'].toString()),
-                 ...respostas.map((t) => Reposta(texto:t, responder:responder)).toList() // spread: todos elementos da lista vao ser adicionados aqui. -> map comunzao: ITERAÇÃO DELCARATIVA
-            ],
-          ) : null
+          body: temPerguntaSelecionada ? Questionario(responder: responder, perguntas: perguntas, perguntaSelecionada: perguntaSelecionada) 
+          : const Resultado(text: 'Parabéns')
         )
-      ); 
+      );
     }
 }
 
